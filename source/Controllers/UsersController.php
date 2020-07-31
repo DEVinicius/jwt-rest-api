5859ca7f7ec426b766a6aee94b2091e555caf0b7
Vinicius Pereira de Oliveira <agavi2014@hotmail.com>
2020-07-31 14:43:43 -0300

    controller

--- source/Controllers/UsersController.php
+++ source/Controllers/UsersController.php
@@ -2,7 +2,50 @@
 
 namespace Source\Controllers;
 
+use Exception;
+use Source\Models\Users;
+
 class UsersController
 {
+    public function create()
+    {
+        $users = new Users();
+
+        $users = new Users();
+        $data = json_decode(file_get_contents("php://input"));
+
+        try 
+        {
+            $users->setName($data->name);
+            $users->setEmail($data->email);
+            $users->setPasswd($data->passwd);
     
+            $users->create();
+        } 
+        catch (Exception $e) 
+        {
+            print_r(
+                json_encode(
+                    [
+                        "error" => $e->getMessage()
+                    ]
+                )
+            );
+        }
+    }
+
+    public function read()
+    {
+        
+    }
+
+    public function update()
+    {
+        $users = new Users();
+    }
+
+    public function delete()
+    {
+        $users = new Users();
+    }
 }
